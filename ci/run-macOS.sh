926df86c061cb87fd24cf3dfd0b15fb0cd754c04
Raphael Isemann <teemperor@gmail.com>
2019-11-13 11:13:22 +0100

    Move to lldb-inst-min tar
    This is a smaller binary that should speed up CI jobs and put
    less stress on my server.

--- ci/run-macOS.sh
+++ ci/run-macOS.sh
@@ -6,8 +6,8 @@ if [[ "$OSTYPE" == "darwin"* ]]; then
   export HOMEBREW_NO_INSTALL_CLEANUP=1
   brew update
   brew install ninja
-  curl https://teemperor.de/pub/lldb-inst.tar.gz --output lldb-inst.tar.gz
-  tar xzf lldb-inst.tar.gz
+  curl https://teemperor.de/pub/lldb-inst-min.tar.xz --output lldb-inst.tar.xz
+  tar xJf lldb-inst.tar.xz
   inst_path=`realpath llvm-inst`
   bash ci/travis.sh "-DCMAKE_PREFIX_PATH=$inst_path"
 else
